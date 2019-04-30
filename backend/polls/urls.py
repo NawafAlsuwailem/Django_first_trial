0a769b95b634335cc125c228db6b52f645376bb7
Nawaf Alsuwailem <nawafalsuwailem@Nawafs-Mac-Pro.local>
2019-04-30 22:39:12 +0300

    third commit

--- backend/polls/urls.py
+++ backend/polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # example: /polls/
+	path('', views.index, name='index'),
+	# example: /polls/id/
+	path('<int:question_id>', views.detail, name="detail"),
+	# example: /polls/id/results/
+	path('<int:question_id>/results/', views.results, name="results"),
+	# example: /polls/id/votes/
+	path('<int:question_id>/vote/', views.vote, name="vote"),
 ]
