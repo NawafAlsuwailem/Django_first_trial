9e07880ba9af2d25e6d853eec2df80c98718171e
Nawaf Alsuwailem <N.Y.Alsuwailem@edu.salford.ac.uk>
2019-05-08 19:52:30 +0300

    forth

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,17 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 
-from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {'latest_question_list': latest_question_list,}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse("You are looking at the question %s." %question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response %question_id)
-
-
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
+	
 def vote(request, question_id):
-	return HttpResponse("You are voting on question %s. " % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		# return the ID of the selected choice as string 
+		# GET can be used too....
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist): # check for primary key existence. 
+		return render(request, 'polls/detail.html',{
+			'question': question,
+			'error_message': "You didn't select a choice",
+		})
+	else:
+		selected_choice.votes+=1
+		selected_choice.save()
+		# return always after sucessful POST request =
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
